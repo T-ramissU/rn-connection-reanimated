8dfdf0698ddc564b858c35d77bfc2ecd20570e0d
Bartłomiej Błoniarz <bartlomiej.bloniarz@swmansion.com>
2025-04-01 08:54:07 +0200

    restore balloonslider

--- src/lessons/2_BalloonSlider/BalloonSlider.tsx
+++ src/lessons/2_BalloonSlider/BalloonSlider.tsx
@@ -1,27 +1,19 @@
-import { AnimatedText } from "@/components/AnimatedText";
 import { Container } from "@/components/Container";
 import { hitSlop } from "@/lib/reanimated";
 import { colorShades, layout } from "@/lib/theme";
-import { StyleSheet, View } from "react-native";
+import { StyleSheet } from "react-native";
 import { Gesture, GestureDetector } from "react-native-gesture-handler";
 import Animated, {
   Extrapolation,
-  clamp,
   interpolate,
-  measure,
-  useAnimatedRef,
   useAnimatedStyle,
-  useDerivedValue,
   useSharedValue,
   withSpring,
 } from "react-native-reanimated";
 
 export function BalloonSliderLesson() {
   const x = useSharedValue(0);
-  const progress = useSharedValue(0);
   const knobScale = useSharedValue(0);
-
-  const aRef = useAnimatedRef<View>();
 
   const panGesture = Gesture.Pan()
     .averageTouches(true)
@@ -29,9 +21,7 @@ export function BalloonSliderLesson() {
       knobScale.value = withSpring(1);
     })
     .onChange((ev) => {
-      const size = measure(aRef);
-      x.value = clamp((x.value += ev.changeX), 0, size.width);
-      progress.value = 100 * (x.value / size.width);
+      x.value += ev.changeX;
     })
     .onEnd(() => {
       knobScale.value = withSpring(0);
@@ -56,48 +46,10 @@ export function BalloonSliderLesson() {
     };
   });
 
-  const balloonSpringyX = useDerivedValue(() => {
-    return withSpring(x.value);
-  });
-
-  const balloonStyle = useAnimatedStyle(() => {
-    return {
-      opacity: knobScale.value,
-      transform: [
-        { translateX: balloonSpringyX.value },
-        { scale: knobScale.value },
-        {
-          translateY: interpolate(
-            knobScale.value,
-            [0, 1],
-            [0, -layout.indicatorSize],
-          ),
-        },
-        {
-          rotate: `${Math.atan2(
-            balloonSpringyX.value - x.value,
-            layout.indicatorSize * 2,
-          )}rad`,
-        },
-      ],
-    };
-  });
-
   return (
     <Container>
       <GestureDetector gesture={panGesture}>
-        <View ref={aRef} style={styles.slider} hitSlop={hitSlop}>
-          <Animated.View style={[styles.balloon, balloonStyle]}>
-            <View style={styles.textContainer}>
-              <AnimatedText
-                text={progress}
-                style={{ color: "white", fontWeight: "600" }}
-              />
-            </View>
-          </Animated.View>
-          <Animated.View style={[styles.progress, { width: x }]} />
-          <Animated.View style={[styles.knob, animatedStyle]} />
-        </View>
+        <Animated.View style={[styles.knob, animatedStyle]} hitSlop={hitSlop} />
       </GestureDetector>
     </Container>
   );
@@ -113,39 +65,5 @@ const styles = StyleSheet.create({
     borderColor: colorShades.purple.base,
     position: "absolute",
     left: -layout.knobSize / 2,
-  },
-  slider: {
-    width: "80%",
-    backgroundColor: colorShades.purple.light,
-    height: 5,
-    justifyContent: "center",
-  },
-  textContainer: {
-    width: 40,
-    height: 60,
-    borderTopLeftRadius: 20,
-    borderTopRightRadius: 20,
-    borderBottomLeftRadius: 40,
-    borderBottomRightRadius: 40,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: colorShades.purple.base,
-    position: "absolute",
-    top: -layout.knobSize,
-  },
-  balloon: {
-    alignItems: "center",
-    justifyContent: "center",
-    width: 4,
-    height: layout.indicatorSize,
-    bottom: -layout.knobSize / 2,
-    borderRadius: 2,
-    backgroundColor: colorShades.purple.base,
-    position: "absolute",
-  },
-  progress: {
-    height: 5,
-    backgroundColor: colorShades.purple.dark,
-    position: "absolute",
   },
 });
